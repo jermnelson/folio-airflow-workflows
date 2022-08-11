4d84750b9d00610556614143c752b5851cee098f
Jeremy Nelson <jermnelson@gmail.com>
2022-08-11 14:19:57 -0600

    Topics content with graphics

---
+++ README.md
@@ -0,0 +1,2 @@
+# FOLIO Bibliographic Workflow Automation with Airflow
+## Presentation for WOLFCon 2022
--- topics.py
+++ topics.py
@@ -7,20 +7,34 @@ from pyodide import create_proxy
 
 
 intro_airflow_template = Template("""<div><h2>What is Airflow?</h2>
-<h3>Used widely in Data Science and Machine Learning Applications</h3>
-<p>
-  
-</p>
+<img src="assets/airflow.png" alt="Airflow Logo" />
 <p>
   <a href="https://airflow.apache.org/">Apache Airflow</a> is an open-source workflow 
   authoring, orchestration and management tool that allows for the construction of 
   complex workflows, called DAGs (short for directed acyclic graphs), that are made up 
-  of one or more code-defined tasks. Airflow has a rich ecosystem of plugins for integrating
-  these tasks with a large number of technologies, both commercial cloud providers like
-  Amazon and Google, along with popular open-source projects like Postgres, .., and ...
+  of one or more code-defined tasks.
 </p>
-<
+<h3>Used widely in Data Science and Machine Learning Applications</h3>
+<p>
+  Companies and organizations in a wide range of industries use Apache Airflow
+  for the following reasons:   
+</p>
+<ul>
+  <li><strong>Ease of use</strong></li>
+  <li><strong>Open Source</strong> with a strong developer and user communities.</li>
+  <li><strong>Integrations</strong>
+   Airflow has a rich ecosystem of plugins for integrating
+   these tasks with a large number of technologies, both commercial cloud providers like
+   Amazon and Google, along with popular open-source projects like Postgres, .., and ...
+   <br>
+   <img src="assets/aws.png" style="height: 75px" alt="AWS Logo"> 
+  </li>
+  <li><strong>Graphical/Web-based User Interface</strong></li>
+  <li><strong>REST API</strong></li>
+</ul>
+<h3>Example DAG with Tasks</h3>
 <pre>
+
 
 
 </pre>
@@ -35,9 +49,16 @@ def intro2airflow(event):
     
 
 
-sinopia_stack_template = Template("""<div><h2>Sinopia Linked-Data Editor</h2>
+sinopia_stack_template = Template("""<div>
+<h2>Sinopia Linked-Data Editor</h2>
 <p>
-  Sinopia, a project of the Linked-Data for Production (LD4), a Mellon funded grant.
+  <a href="https://sinopia.io/">Sinopia</a>, a project of the <a href="http://www.ld4p.org/">Linked-Data for Production (LD4)</a>, 
+  was initially funded by multiple Andrew W. Mellon Foundation grants. 
+</p>
+
+<h3>Sinopia's ILS Middleware</h3>
+<p>
+
 </p>
 </div>""")
 
@@ -49,7 +70,32 @@ def sinopia_stack(event):
     main.innerHTML = sinopia_stack_template.render()
 
 
-marc_migration_template = Template("""<div><h2>MARC BIB Records Migration</h2>
+marc_migration_template = Template("""<div><h2>MARC Records Migration to FOLIO</h2>
+<p>
+  Stanford's migration of bibliographic data from our legacy ILS to FOLIO Library Services Platform,
+  is built with multiple open-source technologies. Our workflow DAGs and plugins are also licensed under
+  the Apache2 license and are available from <a href="https://github.com/sul-dlss/libsys-airflow/">LibSys Airflow</a>
+  repository.
+</p>
+<h3>FOLIO-FSE Migration Tools</h3>
+<p>
+  Stanford's DAGs use the open-source tools  <a href="https://github.com/FOLIO-FSE/folio_migration_tools/">FOLIO Migration Tools</a> 
+  and <a href="https://github.com/FOLIO-FSE/FolioClient">FolioClient</a> developed by Ebsco's FOLIO-FSE team for the majority of 
+  the core migration from MARC21 binary and tab-separated files (TSV) to FOLIO Instances, Holdings, and Items records. 
+</p>
+<h2>Directed Acyclic Graph Workflows</h2>
+<h3><em>symphony_marc_import</em> DAG</h3>
+<p>
+  The primary 
+</p>
+<h3><em>ingest_marc_to_srs</em> DAG</h3>
+<h3><em>fix_failed_record_loads</em>DAG</h3>
+<h2>Recent Functionality</h2>
+
+<h3>Boundwidths, Proceeding/Succeeding Titles</h3>
+
+<h3>MHLD</h3>
+
 
 </div>""")
 
@@ -61,9 +107,14 @@ def marc_migration(event):
     main.innerHTML = marc_migration_template.render()
 
 future_workflows_template = Template("""<div><h2>Future Workflows</h2>
+<h3>Circulation Migration: Loans and Requests</h3> 
 <p>
 
 </p>
+<h3>Migrating Bills</h3>
+<h3>Ongoing MARC Record and User Loads</h3>
+<h3>Authority Records</h3>
+<h3>Further Sinopia Integration</h3>
 </div>""")
 
 def future_workflows(event):
