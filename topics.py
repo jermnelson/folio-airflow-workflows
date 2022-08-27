146d14ece09cece9a03e70e4633b13be7af60c3f
Jeremy Nelson <jermnelson@gmail.com>
2022-08-27 11:44:18 -0700

    Content addition with graphics

--- topics.py
+++ topics.py
@@ -17,10 +17,10 @@ with open("sinopia-stack.html") as fo:
 with open("future-workflows.html") as fo:
     future_workflows_template = Template(fo.read())
 
-def _render_template(subtitle, template):
+def _render_template(subtitle_text, template):
     main = js.document.getElementById('content')
     subtitle = js.document.getElementById('subtitle')
-    subtitle.innerHTML = subtitle
+    subtitle.innerHTML = subtitle_text
     main.innerHTML = template.render()
 
 
@@ -28,23 +28,11 @@ def intro2airflow(event):
     _render_template("-- Introduction to Airflow", intro_airflow_template)
 
 def sinopia_stack(event):
-    main = js.document.getElementById('content')
-    subtitle = js.document.getElementById('subtitle')
-    subtitle.innerHTML = "-- Sinopia Editor Stack"
- 
-    main.innerHTML = sinopia_stack_template.render()
+    _render_template("-- Sinopia Editor Stack", sinopia_stack_template)
 
 def marc_migration(event):
-    main = js.document.getElementById('content')
-    subtitle = js.document.getElementById('subtitle')
-    subtitle.innerHTML = "-- MARC BIB Record Migration"
- 
-    main.innerHTML = marc_migration_template.render()
+    _render_template("-- MARC BIB Record Migration", marc_migration_template)
 
 
 def future_workflows(event):
-    main = js.document.getElementById('content')
-    subtitle = js.document.getElementById('subtitle')
-    subtitle.innerHTML = "-- Future Workflows"
- 
-    main.innerHTML = future_workflows_template.render()
+    _render_template("-- Future Workflows", future_workflows_template)
